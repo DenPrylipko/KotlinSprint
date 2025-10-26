e1a15f90e8d97b4ed52944fb36b34457989b8575
DenPrylipko <denprylipko09@gmail.com>
2025-10-26 23:51:58 +0200

    KS-1-2 added details

---
+++ src/main/kotlin/task2.kt
@@ -0,0 +1,16 @@
+package org.example
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val gratitudeText: String = "Thank you for your trust, we work for you :)"
+
+    var employees: Int = 2000
+
+    println(numberOfOrders)
+    println(gratitudeText)
+//    println(employees)
+
+    employees -= 1
+
+    println(employees)
+}
