9c8faf2887522f37772c5ddab78b408ab9286983
DenPrylipko <denprylipko09@gmail.com>
2025-10-29 18:41:34 +0200

    KS-2-3 correction the task

--- src/main/kotlin/lesson2/task3.kt
+++ src/main/kotlin/lesson2/task3.kt
@@ -1,10 +1,11 @@
 package org.example.lesson2
 
-import org.example.lesson1.MINUTES_IN_AN_HOUR_OR_SECONDS_IN_A_MINUTE
-
 fun main() {
 
-    val trainLeftTime = 9 * MINUTES_IN_AN_HOUR_OR_SECONDS_IN_A_MINUTE + 39
+    val hourWhenTrainLeft = 9
+    val minuteWhenTrainLeft = 39
+
+    val trainLeftTime = hourWhenTrainLeft * MINUTES_IN_AN_HOUR_OR_SECONDS_IN_A_MINUTE + minuteWhenTrainLeft
     val minutesTillArrive = 457
 
     val hoursComingTime = (trainLeftTime + minutesTillArrive) / MINUTES_IN_AN_HOUR_OR_SECONDS_IN_A_MINUTE
@@ -13,7 +14,6 @@ fun main() {
     val comingTime = "%02d:%02d".format(hoursComingTime, minutesComingTime)
 
     println(comingTime)
-
-
-
 }
+
+const val MINUTES_IN_AN_HOUR_OR_SECONDS_IN_A_MINUTE = 60
