d0e7dd967247383ab515b7afcd1fb137769288d9
DenPrylipko <denprylipko09@gmail.com>
2025-10-30 20:19:55 +0200

    KS-3-3 Multiplication table correction

--- src/main/kotlin/lesson3/task3.kt
+++ src/main/kotlin/lesson3/task3.kt
@@ -5,15 +5,15 @@ fun main() {
     val digit = 5
 
     val multiplicationTable = """
-        ${digit * 1} * 1 = ${digit * 1}
-        ${digit * 2} * 2 = ${digit * 2}
-        ${digit * 3} * 3 = ${digit * 3}
-        ${digit * 4} * 4 = ${digit * 4}
-        ${digit * 5} * 5 = ${digit * 5}
-        ${digit * 6} * 6 = ${digit * 6}
-        ${digit * 7} * 7 = ${digit * 7}
-        ${digit * 8} * 8 = ${digit * 8}
-        ${digit * 9} * 9 = ${digit * 9}
+        $digit * 1 = ${digit * 1}
+        $digit * 2 = ${digit * 2}
+        $digit * 3 = ${digit * 3}
+        $digit * 4 = ${digit * 4}
+        $digit * 5 = ${digit * 5}
+        $digit * 6 = ${digit * 6}
+        $digit * 7 = ${digit * 7}
+        $digit * 8 = ${digit * 8}
+        $digit * 9 = ${digit * 9}
     """.trimIndent()
 
     println(multiplicationTable)
