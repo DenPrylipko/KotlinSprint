b9534fe4836928acf4384d2112ac13ee1e1bd55c
DenPrylipko <denprylipko09@gmail.com>
2025-11-01 14:30:59 +0200

    KS-4-1 correcting

--- src/main/kotlin/lesson4/task1.kt
+++ src/main/kotlin/lesson4/task1.kt
@@ -1,13 +1,13 @@
 package org.example.lesson4
 
 fun main() {
-    val tablesInRestaurant = 13
-
-    val reservedTablesForToday = tablesInRestaurant
+    val reservedTablesForToday = 13
     val reservedTablesForTomorrow = 9
 
-    val freeTablesForToday = reservedTablesForToday < tablesInRestaurant
-    val freeTablesForTomorrow = reservedTablesForTomorrow < tablesInRestaurant
+    val freeTablesForToday = reservedTablesForToday < TABLES_IN_RESTAURANT
+    val freeTablesForTomorrow = reservedTablesForTomorrow < TABLES_IN_RESTAURANT
 
     println("Free tables for today: $freeTablesForToday \nFree tables for tomorrow: $freeTablesForTomorrow")
 }
+
+const val TABLES_IN_RESTAURANT = 13
