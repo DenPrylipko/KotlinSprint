9ea5555586180f2561b71fd34792b0de6310fdeb
DenPrylipko <denprylipko09@gmail.com>
2025-11-02 13:08:48 +0200

    KS-4-3 derived constants and changed and changed variable name

--- src/main/kotlin/lesson4/task3.kt
+++ src/main/kotlin/lesson4/task3.kt
@@ -2,12 +2,18 @@ package org.example.lesson4
 
 fun main() {
 
-    val sunnyWeather = true
-    val tentIsOpened = true
+    val isSunnyWeather = true
+    val isTentOpen = true
     val airHumidity = 20
     val yearSeason = "winter"
 
-    val result = (sunnyWeather == true) && (tentIsOpened == true) && (airHumidity == 20) && (yearSeason != "winter")
+    val result = (isSunnyWeather == SUNNY_WEATHER) && (isTentOpen == TENT_IS_OPENED)
+            && (airHumidity == FAVORABLE_AIR_HUMIDITY) && (yearSeason != NOT_FAVORABLE_YEAR_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
 }
+
+const val SUNNY_WEATHER = true
+const val TENT_IS_OPENED = true
+const val FAVORABLE_AIR_HUMIDITY = 20
+const val NOT_FAVORABLE_YEAR_SEASON = "winter"
