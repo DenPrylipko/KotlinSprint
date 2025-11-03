6fe3c6a6c15ed945603683d83af95f51759848f4
DenPrylipko <denprylipko09@gmail.com>
2025-11-03 17:04:52 +0200

    KS-5-3 added random numbers and corrected logic

--- src/main/kotlin/lesson5/task3.kt
+++ src/main/kotlin/lesson5/task3.kt
@@ -2,8 +2,8 @@ package org.example.lesson5
 
 fun main() {
 
-    val firstNumber = 12
-    val secondNumber = 9
+    val firstNumber = (0..42).random()
+    val secondNumber = (0..42).random()
 
     val oneNumberGuessedText = "You won consolation prize :|"
     val twoNumbersGuessedText = "Congratulation! You won a Legendary Prize"
@@ -11,34 +11,33 @@ fun main() {
     val afterText = "Win numbers were $firstNumber and $secondNumber\nThank you for playing>"
 
     println(
-        "This is a lottery." +
-                "\nThere are 2 numbers between 0 and 42, guess them both to win a Legendary Prize." +
-                "\nIf you guess one number - you will get a consolation prize." +
-                "\n\n Enter your numbers..."
+        """
+        This is a lottery.
+        There are 2 numbers between 0 and 42, guess them both to win a Legendary Prize.
+        If you guess one number - you will get a consolation prize.
+        
+        Enter your numbers...
+    """.trimIndent()
     )
 
     val firstUserNumber = readln().toInt()
+
     println("Enter your second number...")
     val secondUserNumber = readln().toInt()
 
     if ((firstUserNumber == firstNumber || firstUserNumber == secondNumber) &&
-            (secondUserNumber == firstNumber || secondUserNumber == secondNumber)
-    ) {
-        println(twoNumbersGuessedText)
-        println(afterText)
-    } else if ((firstUserNumber == firstNumber || firstUserNumber == secondNumber) &&
-        (secondUserNumber != firstNumber || secondUserNumber != secondNumber)
-    ) {
-        println(oneNumberGuessedText)
-        println(afterText)
-    } else if ((firstUserNumber != firstNumber || firstUserNumber != secondNumber) &&
         (secondUserNumber == firstNumber || secondUserNumber == secondNumber)
     ) {
+        println(twoNumbersGuessedText)
+    } else if ((firstUserNumber == firstNumber || firstUserNumber == secondNumber)
+    ) {
         println(oneNumberGuessedText)
-        println(afterText)
+    } else if ((secondUserNumber == firstNumber || secondUserNumber == secondNumber)
+    ) {
+        println(oneNumberGuessedText)
     } else {
         println(noneGuessed)
-        println(afterText)
     }
 
+    println(afterText)
 }
